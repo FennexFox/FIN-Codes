be6f459358df51a164f0ecc997e563172aa813ae
AnotherFireFox <technocracy90@gmail.com>
2023-05-15 15:06:19 +0900

    Create String

---
+++ BasicLibraries/String.lua
@@ -0,0 +1,16 @@
+String = {}
+
+String = {}
+function String.addLine(string, line)
+    if string == {} then string = line else string = string .. "\n" .. line end
+    return string
+end
+
+function String.KeyGenerator(name)
+    local key = string.gsub(name, "Alternate: ", "A_ ")
+    key = string.gsub(key, "Iron", "Irn ")
+    key = string.gsub(key, "Copper", "Cpr ")
+    key = string.gsub(key, "Ingot", "Ing ")
+    key = string.gsub(key, " ", "")
+    return key
+end
