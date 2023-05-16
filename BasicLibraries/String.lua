5b28c218990af3a12b8a13e37fb39e1b5de33d35
AnotherFireFox <technocracy90@gmail.com>
2023-05-16 23:15:14 +0900

    Add KeyGenerator

--- BasicLibraries/String.lua
+++ BasicLibraries/String.lua
@@ -1,4 +1,5 @@
 String = {}
+-- String.Keys = {["Alternate: "] = "A_ ", ["Iron"] = "Irn ", ["Copper"] = "Cpr ", ["Ingot"] = "Igt ", ["Smart"] = "Smrt ", ["Automated"] = "Atm "}
 
 function String.addLine(string, line)
     if string == {} then string = line else string = string .. "\n" .. line end
@@ -6,10 +7,15 @@ function String.addLine(string, line)
 end
 
 function String.KeyGenerator(name)
-    local key = string.gsub(name, "Alternate: ", "A_ ")
+    local key = name
+
+    key = string.gsub(key, "Alternate: ", "A_ ")
     key = string.gsub(key, "Iron", "Irn ")
     key = string.gsub(key, "Copper", "Cpr ")
-    key = string.gsub(key, "Ingot", "Ing ")
+    key = string.gsub(key, "Ingot", "Igt ")
+    key = string.gsub(key, "Smart", "Smrt ")
+    key = string.gsub(key, "Automated", "Atm ")
+
     key = string.gsub(key, " ", "")
     return key
 end
