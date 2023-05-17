4b014928b6d0e47037340a43960573ebafc4e7a2
AnotherFireFox <technocracy90@gmail.com>
2023-05-17 11:14:42 +0900

    Reconstruct RecipeTree
    - Change NewRecipeNode() to NewNode()
    - Remove throughput and Set self[rkey][fkey][ikey] directly

--- ProductionLineControl/RecipeTree.lua
+++ ProductionLineControl/RecipeTree.lua
@@ -9,12 +9,7 @@ function RecipeTree:New()
         
     end
 
-    function RecipeTree:NewThroughtputNode(recipeNode, itemAmount, isIngredient)
-        local throughtput = {Name = itemAmount.Type.Name, Amount = itemAmount.Amount, Duration = recipeNode.Duration, Recipe = recipeNode, IsIngredient = isIngredient}
-        return throughtput
-    end
-    
-    function RecipeTree:NewRecipeNode(recipeInstance)
+    function RecipeTree:NewNode(recipeInstance)
         local recipe = recipeInstance
         local status, rkey = pcall(String.KeyGenerator, recipe.Name)
         local iCounter, pCounter = 0, 0
@@ -22,18 +17,18 @@ function RecipeTree:New()
         if not status then error("Cannot find Name of the Recipe!")
         elseif self[rkey] then error(recipe.Name .. " already listed; abort adding RecipeNode")
         else
-            self[rkey] = {Name = recipe.Name, Duration = recipe.Duration, Ingredients = {}, Products = {}, Recipe = recipe}
+            self[rkey] = {Name = recipe.Name, Duration = recipe.Duration, Inflows = {}, Outflows = {}, Recipe = recipe}
             print(recipe.Name .. " detected; cacheing ingredients and products")
             local ingredients, products = recipe:getIngredients(), recipe:getProducts()
 
             for _, i in pairs(ingredients) do
                 local ikey = String.KeyGenerator(i.Type.Name)
-                self[rkey].Ingredients[ikey] = self:NewThroughtputNode(self[rkey], i, true)
+                self[rkey].Ingredients[ikey] = self:SetThroughtputs(self[rkey], i, true)
                 iCounter = iCounter + 1
             end
             for _, p in pairs(products) do
                 local ikey = String.KeyGenerator(p.Type.Name)
-                self[rkey].Products[ikey] = self:NewThroughtputNode(self[rkey], p, nil)
+                self[rkey].Products[ikey] = self:SetThroughtputs(self[rkey], p, nil)
                 pCounter = pCounter + 1
             end
 
@@ -45,11 +40,18 @@ function RecipeTree:New()
         for _, v in ipairs(machineProxies) do
             local status, recipeInstance = pcall(v.getRecipe, v)
             if not status then error(v.internalName .. " has no Recipe!")
-            else self:NewRecipeNode(recipeInstance)
+            else self:NewNode(recipeInstance)
             end
         end
 
         print("RecipeNode Cacheing Complete") return true
+    end
+
+    function RecipeTree:SetThroughputs(recipeNode, itemAmount, isIngredient)
+        local rkey, ikey, fkey = String.KeyGenerator(recipeNode.Name), String.KeyGenerator(itemAmount.Type.Name), ""
+        if isIngredient then fkey = "Inflows" else fkey = "Outflows" end
+
+        self[rkey][fkey][ikey] = {Name = itemAmount.Type.Name, Amount = itemAmount.Amount, Duration = recipeNode.Duration, Recipe = recipeNode, IsIngredient = isIngredient}
     end
 
     setmetatable(instance, {__index = self})
