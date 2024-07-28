56a21f417d4194bbfabb786b96720d7fbda78055
AnotherFireFox <technocracy90@gmail.com>
2024-07-28 15:18:12 +0900

    Delete duplicated String.KeyGenerator()

--- BasicLibraries/String.lua
+++ BasicLibraries/String.lua
@@ -31,20 +31,6 @@ function String.KeyGenerator(name)
     return key
 end
 
-function String.ItemKeyGenerator(itemType)
-    local name, key = String.KeyGenerator(itemType.name), ""
-    local size = string.format("%02.0f", itemType.max/50)
-
-    for v in name:gmatch("%u%U?%U?") do -- finding 3~1 letters starts with a uupercase and then lowercases
-        local keyLen = string.len(key:match("%u+") or "")
-        if keyLen > 3 then break end -- if key is already longer than 3 letters, abort
-        key = key:sub(1, keyLen) .. v -- if not, make it longer
-    end
-
-    key = key:upper() .. "xx" -- if key is still shorter than 3 letters, put placeholder
-    return size .. key:sub(1, 3)
-end
-
 function String.NameParser(name)
     local nodeType = name:gsub("%[(.*)%]_(.*)", "%1")
     local nodeKey = name:gsub("%[(.*)%]_(.*)", "%2")
